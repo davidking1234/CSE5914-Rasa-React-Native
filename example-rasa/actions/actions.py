babf1ecfc89edc797b4c6e127a0cd60c07242134
Avi Parshionikar <avipa1@Avis-MBP.lan>
2023-04-17 17:13:21 -0400

    fixed chatbot/moved things to actions

--- example-rasa/actions/actions.py
+++ example-rasa/actions/actions.py
@@ -46,6 +46,16 @@ class ClearRoundLigamentSlot(Action):
 
         return[SlotSet('round_ligament', None)]  
     
+class ClearBackPainSlot(Action):
+    def name(self) -> Text:
+        return 'action_clear_back_pain_slot'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        return[SlotSet('back_pain', None)]  
+    
 class ClearTypesTopicSlot(Action):
     def name(self) -> Text:
         return 'action_clear_types_topic_slot'
@@ -66,6 +76,220 @@ class ClearInfluenceTopicSlot(Action):
         domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
 
         return[SlotSet('influence_topic', None)]  
+    
+
+class UtterTypes(Action):
+    def name(self) -> Text:
+        return 'action_types'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+
+        print("UTTER TYPES types: ",types, "influence: ", influence)
+        #if(types is not None and influence is None):
+        dispatcher.utter_message(response = "utter_body_aches_types")
+
+
+        return None  
+    
+class UtterInfluences(Action):
+    def name(self) -> Text:
+        return 'action_influence'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+
+        influence = tracker.get_slot('influence_topic')
+        print("UTTER INFLUENCE influence: ",influence, "types: ", types)
+        #if(influence is not None and types is None):
+        dispatcher.utter_message(response = "utter_body_aches_influence_types")
+
+        return None  
+    
+class SciaticaDefinition(Action):
+    def name(self) -> Text:
+        return 'action_sciatica_types'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+        sciatica = tracker.get_slot('sciatica')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+        print("SCIATICA_DEF: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back_pain: ", back_pain)
+        #if (types is not None and sciatica is not None and carpal_tunnel is None and influence is None):
+        dispatcher.utter_message(response = "utter_body_aches_sciatica_definition")
+        
+       
+        return None
+    
+class CarpalTunnelDefinition(Action):
+    def name(self) -> Text:
+        return 'action_carpal_tunnel_types'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+        sciatica = tracker.get_slot('sciatica')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+        print("CARPAL_TUNNEL_DEF: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back_pain: ", back_pain)
+
+        #if (types is not None and carpal_tunnel is not None and influence is None and sciatica is None):
+        dispatcher.utter_message(response = "utter_body_aches_carpal_tunnel_definition")
+       
+
+        return None
+    
+class RoundLigamentDefinition(Action):
+    def name(self) -> Text:
+        return 'action_round_ligament_types'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+        sciatica = tracker.get_slot('sciatica')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+        print("ROUND_LIGAMENT_DEF: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back_pain: ", back_pain)
+        #if (types is not None and carpal_tunnel is not None and influence is None and sciatica is None):
+        dispatcher.utter_message(response = "utter_body_aches_round_ligament_definition")
+        
+
+        return None
+    
+class BackPainDefinition(Action):
+    def name(self) -> Text:
+        return 'action_back_pain_types'
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        sciatica = tracker.get_slot('sciatica')
+        influence = tracker.get_slot('influence_topic')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+
+        print("BACK_PAIN_DEF: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round ligament: ", round_ligament, "back pain: ", back_pain)
+
+        #if (types is not None and carpal_tunnel is not None and influence is None and sciatica is None):
+        dispatcher.utter_message(response = "utter_body_aches_back_pain_definition")
+        
+        return None
+    
+class SciaticaInfluence(Action):
+    def name(self) -> Text:
+        return 'action_sciatica_influence'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+        sciatica = tracker.get_slot('sciatica')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+        print("SCIATICA_INFLUENCE: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back_pain: ", back_pain)
+        #if (types is not None and sciatica is not None and carpal_tunnel is None and influence is None):
+        dispatcher.utter_message(response = "utter_body_aches_influencing_factors_sciatica")
+        
+       
+        return None
+    
+class CarpalTunnelInfluence(Action):
+    def name(self) -> Text:
+        return 'action_carpal_tunnel_influence'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+        sciatica = tracker.get_slot('sciatica')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+        print("CARPAL_TUNNEL_INFLUENCE: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back_pain: ", back_pain)
+
+        #if (types is None and carpal_tunnel is not None and influence is not None and sciatica is None):
+        dispatcher.utter_message(response = "utter_body_aches_influencing_factors_carpal_tunnel")
+           
+        return None
+    
+class RoundLigamentInfluence(Action):
+    def name(self) -> Text:
+        return 'action_round_ligament_influence'
+    
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        influence = tracker.get_slot('influence_topic')
+        sciatica = tracker.get_slot('sciatica')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+        print("ROUND_LIGAMENT_ INFLUENCE: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back_pain: ", back_pain)
+        
+        #if (types is None and carpal_tunnel is None and influence is not None and sciatica is None and round_ligament is not None):
+        dispatcher.utter_message(response = "utter_body_aches_influencing_factors_round_ligament")
+           
+        return None
+    
+
+class BackPainInfluence(Action):
+    def name(self) -> Text:
+        return 'action_back_pain_influence'
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any])-> List[Dict[Text, Any]]:
+
+        types = tracker.get_slot('types_topic')
+        sciatica = tracker.get_slot('sciatica')
+        influence = tracker.get_slot('influence_topic')
+        carpal_tunnel = tracker.get_slot('carpal_tunnel')
+        round_ligament = tracker.get_slot('round_ligament')
+        back_pain = tracker.get_slot('back_pain')
+
+
+        print("BACK_PAIN_INFLUENCE: types: ",types,"influence: ", influence, "sciatica: ", sciatica, "carpal_tunnel: ", carpal_tunnel, "round_ligament: ", round_ligament, "back pain: ", back_pain)
+
+        #if (types is not None and carpal_tunnel is not None and influence is None and sciatica is None):
+        dispatcher.utter_message(response = "utter_body_aches_influencing_factors_back_pain")
+        
+        return None
     
 class ActionElasticSearch(Action):
     def name(self) -> Text:
